d3b3a1188ec23d3d79e2bd89586ed6f78e959b6c
NitinSingh99 <8761964643ns@gmail.com>
2025-12-27 22:52:07 +0530

    update

--- src/js/main.js
+++ src/js/main.js
@@ -80,4 +80,10 @@ function initThemeToggle() {
     });
 }
 
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
 
