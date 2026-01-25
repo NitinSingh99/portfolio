3d0173d2f7c9336607154c4bf39b9ca54bfb95af
Nitinsingh99 <8761964643ns@gmail.com>
2026-01-25 16:06:05 +0530

    update

---
+++ src/components/experience.js
@@ -0,0 +1,143 @@
+export function initExp() {
+    const expSection = document.getElementById("workExp");
+    expSection.innerHTML = `<section id="experience" class="experience">
+        <div class="w-full px-4 md:px-8 lg:px-16">
+          <div class="w-full">
+            <div class="flex items-center gap-2 block-header-title max-w-7xl mx-auto py-12 lg:py-20">
+              <!-- <span class="w-2.5 h-2.5 lg:w-2.5 lg:h-2.5 rounded-full 
+           bg-black dark:bg-white 
+           translate-y-[0.04em]">
+              </span> -->
+              <p class="font-bold lg:font-extrabold text-xl md:text-2xl lg:text-2xl text-zinc-800 dark:text-zinc-100">
+                Work Experience
+              </p>
+            </div>
+            <div class="relative max-w-7xl mx-auto pb-4">
+              <div class="flex justify-start pt-6 md:pt-15 md:gap-10">
+                <div
+                  class="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+                  <div
+                    class="absolute h-6 w-6 lg:h-10 lg:w-10 left-5 md:left-5 lg:left-3 rounded-full bg-zinc-800 dark:bg-black flex items-center justify-center">
+                    <div class="h-2 w-2 lg:h-4 lg:w-4 rounded-full bg-white dark:bg-neutral-800">
+                    </div>
+                  </div>
+                  <h3 class="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-zinc-800 dark:text-zinc-100">
+                    National Informatics Centre</h3>
+                </div>
+                <div class="relative pl-20 pr-4 md:pl-4 w-full">
+                  <h3 class="md:hidden block text-xl mb-6 text-left font-bold text-zinc-800 dark:text-zinc-100">
+                    National Informatics Centre</h3>
+                  <div>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">
+                      Led development of a Marriage service platform that reduced processing time
+                      by 40%, handling 30,000+ monthly registrations with microservices
+                      architecture and maintaining 99.9% uptime.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">
+                      Architected and developed a paperless document registration that eliminated
+                      95% of paper usage and decreased processing time from days to hours.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">
+                      Built RESTful APIs that integrated 5 government services, enabling 10,000+
+                      daily transactions and reducing manual data entry by 85%.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">
+                      Developed an interactive admin dashboard that provided monitoring
+                      capabilities across 75+ offices.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-6">Nov 2023 -
+                      Present</p>
+                  </div>
+                </div>
+              </div>
+              <div class="flex justify-start pt-10 md:pt-40 md:gap-10">
+                <div
+                  class="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+                  <div
+                    class="absolute h-6 w-6 lg:h-10 lg:w-10 left-5 md:left-5 lg:left-3 rounded-full bg-zinc-800 dark:bg-black flex items-center justify-center">
+                    <div class="h-2 w-2 lg:h-4 lg:w-4 rounded-full bg-white dark:bg-neutral-800">
+                    </div>
+                  </div>
+                  <h3 class="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-zinc-800 dark:text-zinc-100">
+                    GITCS</h3>
+                </div>
+                <div class="relative pl-20 pr-4 md:pl-4 w-full">
+                  <h3 class="md:hidden block text-xl mb-6 text-left font-bold text-zinc-800 dark:text-zinc-100">
+                    GITCS</h3>
+                  <div>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Refactored the
+                      website frontend using HTML5, CSS3, and Bootstrap to standardize UI components</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Improved
+                      mobile responsiveness by 75% across devices and screen sizes.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Reduced bounce
+                      rate by 30% by enhancing layout consistency and user experience.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Ensured
+                      cross-browser compatibility and improved overall frontend maintainability.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-6">April 2023 -
+                      June 2023</p>
+                  </div>
+                </div>
+              </div>
+              <div class="flex justify-start pt-10 md:pt-40 md:gap-10">
+                <div
+                  class="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+                  <div
+                    class="absolute h-6 w-6 lg:h-10 lg:w-10 left-5 md:left-5 lg:left-3 rounded-full bg-zinc-800 dark:bg-black flex items-center justify-center">
+                    <div class="h-2 w-2 lg:h-4 lg:w-4 rounded-full bg-white dark:bg-neutral-800">
+                    </div>
+                  </div>
+                  <h3 class="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-zinc-800 dark:text-zinc-100">
+                    NEILIT</h3>
+                </div>
+                <div class="relative pl-20 pr-4 md:pl-4 w-full">
+                  <h3 class="md:hidden block text-xl mb-6 text-left font-bold text-zinc-800 dark:text-zinc-100">
+                    NEILIT</h3>
+                  <div>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Completed a
+                      two-month Data Science training program focused on MySQL and MongoDB.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Gained
+                      hands-on experience in database design, querying, and data manipulation.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Worked with
+                      structured and unstructured data using relational and NoSQL databases.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-4">Applied
+                      concepts through practical exercises and mini projects.</p>
+                    <p class="text-zinc-600 dark:text-zinc-400 mb-6">Jan 2023 -
+                      March 2023</p>
+                  </div>
+                </div>
+              </div>
+
+              <!-- TIMELINE -->
+              <div id="timeline" class="absolute md:left-8 left-8 top-0 overflow-hidden w-0.5
+                  bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))]
+                from-transparent via-neutral-200 dark:via-neutral-700 to-transparent
+                mask-[linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]"
+                style="height: 100%;">
+
+                <!-- PROGRESS LINE -->
+                <div id="timeline-progress" class="absolute inset-x-0 top-0 w-[2px]
+                  bg-linear-to-b
+                  from-teal-400 via-teal-500 to-cyan-500
+                  rounded-full
+                  shadow-[0_0_10px_rgba(20,184,166,0.6)]
+                  transition-[height] duration-200 ease-out" style="height: 0%;">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>`;
+
+    const section = document.getElementById("experience");
+    const timeline = document.getElementById("timeline-progress");
+
+    window.addEventListener("scroll", () => {
+        const rect = section.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        const total = rect.height - windowHeight;
+        const scrolled = Math.min(
+            Math.max(-rect.top, 0),
+            total
+        );
+
+        const progress = (scrolled / total) * 100;
+        timeline.style.height = `${progress}%`;
+    });
+}
